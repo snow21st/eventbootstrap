3bf836bf344d9907384c1c35780328041c265ace
bobo <bobo.im2@gmail.com>
2020-08-10 09:07:02 +0630

    event

---
+++ custom.js
@@ -0,0 +1,22 @@
+$(window).scroll(function () {
+	$('nav').toggleClass('scrolled',$(this).scrollTop()>30);
+});
+
+
+
+	 $(".gallery-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    center:true,
+    responsive: { 0: { items: 1 }, 768: { items: 3 }, 992: { items: 4 }, 1200: {items: 5}
+    }
+  });
+
+  // Buy tickets select the ticket type on click
+  $('#buy-ticket-modal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget);
+    var ticketType = button.data('ticket-type');
+    var modal = $(this);
+    modal.find('#ticket-type').val(ticketType);
+  })
